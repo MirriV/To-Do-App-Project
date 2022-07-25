fc40b34eed528d7227f3b51040061e47f95fbaea
MirriV <101925718+MirriV@users.noreply.github.com>
2022-07-25 21:09:45 +0200

    template try outs

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const input = document.querySelector(".taskInput input");
-const button = document.querySelector(".taskInput button");
+//Create a new list item when clicking on new task
 
-//user clicks on add new task//
-button.onclick=()=>{
-    let userData= input.value; //getting user entered value//
-    let getLocalStorage=localStorage.getItem("New Todo");
-    if(getLocalStorage==null){
-        listArr=[];
-    }else{
-        listArr=JSON.parse(getLocalStorage); //transforming JSON string into a JS object//
-    }
-    listArr.push("Hello World!"); //pushing or adding user data//
-    localStorage.setItem("New Todo", JSON.stringify(listArr));//transforming JS object into a JSON//
+let addToDoButton= document.getElementById('addItem');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
 
-}
+addToDoButton.addEventListener('click',function(){
+    var paragraph = document.createElement('p')
+    paragraph.innerText=inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value=""; //clear input field after adding new task
+
+})
