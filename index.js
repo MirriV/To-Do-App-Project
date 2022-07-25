b9dc487565b1be685bc3c72512b10c772958dca4
MirriV <101925718+MirriV@users.noreply.github.com>
2022-07-25 22:07:13 +0200

    Trying to add edit & delete buttons when list item added

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ let addToDoButton= document.getElementById('addItem');
 let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
 
+let editButton=document.getElementById('editButton');
+let deleteButton=document.getElementById('deleteButton');
+
 addToDoButton.addEventListener('click',function(){
     var paragraph = document.createElement('p')
     paragraph.innerText=inputField.value;
     toDoContainer.appendChild(paragraph);
     inputField.value=""; //clear input field after adding new task
+
+  
 
 })
