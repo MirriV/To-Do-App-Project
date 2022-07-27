779c66fc52d9b0c26a09bb710c9b4ff9bfac5f62
MirriV <101925718+MirriV@users.noreply.github.com>
2022-07-27 22:55:21 +0200

    JS start continued

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
 //Create a new list item when clicking on new task
 //Selectors
-let todoInput = document.getElementById('inputField');
-let todoButton= document.getElementById('addItem');
-let todoList=document.getElementById('todo-list');
+let todoInput = document.querySelector('.todo-input');
+let todoButton = document.querySelector('.todo-button');
+let todoList = document.querySelector('todo-input');
 
 let toDoContainer = document.getElementById('toDoContainer');
 
 //Event Listeners
-addToDoButton.addEventListener('click',addTodo);
+addtodoButton.addEventListener('click', addTodo);
 
 //functions
 
-function addTodo(event){
+function addTodo(event) {
     //Prevent form from refreshing/submitting
     event.preventDefault();
-    //Todo Div
-    const todoDiv =document.createElement('div');
+    //Todo Div to which buttons are appended
+    const todoDiv = document.createElement('div');
     todoDiv.classList.add('todo');
 
-//Create LI
-const newTodo=document.createElement('li');
-newTodo.innerText='hey';
-newTodo.classList.add('todo-item');
-todoDiv.appendChild(newTodo);
-//checked button
-const completedButton=document.createElement('button');
-completedButton.innerHTML= '<i class="fas fa-check"></i>';
-completedButton.classList.add("complete-btn");
-todoDiv.appendChild(completedButton);
-//Delete button
-const trashButton=document.createElement('button');
-trashButton.innerHTML= '<i class="fas fa-check"></i>';
-trashButton.classList.add("complete-btn");
-todoDiv.appendChild(trashButton);
+    //Create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //checked button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //Delete button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("complete-btn");
+    todoDiv.appendChild(trashButton);
+
+    //append to list(attaching to actual UL)
+    todoList.appendChild(todoDiv);
 
 }
 
