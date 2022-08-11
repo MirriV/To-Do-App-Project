decdc9709df4819d8e381d89d0921569131dcce5
MirriV <101925718+MirriV@users.noreply.github.com>
2022-08-11 20:21:40 +0200

    changes to sort function & final updates before submitting

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+
 //Create a new list item when clicking on new task
 //Selectors
 let todoInput = document.querySelector('.todo-input');
 let addtodoButton = document.querySelector('.todo-button');
 let todoList = document.querySelector('.todo-list');
 
-
+/*Sort Button*/
+const sortButton = document.querySelector('#sort-button');
 
 
 
@@ -13,32 +15,68 @@ document.addEventListener("DOMContentLoaded", getTodos);
 addtodoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
 
+sortButton.addEventListener("click", function (e) {
 
-/*Sort function*/
+    // stop form from submitting 
+    e.preventDefault();
 
-function sortList(){
-    
-    var list, i , switching, listitems, shouldSwitch;
-    list = document.getElementById('todoSort');
-    switching= true;
+    sortList();
 
-    while (switching) {
-        switching=false;
-        listitems =list.getElementsByTagName("li");
+});
 
-        for (i=0 ;i<(listitems.lenght-1); i++){
-            shouldSwitch =false;
-            if(listitems[i].innerHTML.toLowerCase()>listitems[i+1].innerHTML.toLowerCase)
-            shouldSwitch=true;
-            break;
 
-        }
+// Sort and Display function:
+function sortList() {
+
+    // clear all elements inside todoList element
+    todoList.innerHTML = "";
+
+    let todos = [];
+
+    if (localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
     }
-    listitems[i].parentNode.insertBefore(listitems[i+1], listitems[i]);
-    switching =true;
+
+    // sort function to sort array of primitive types
+    todos.sort();
+
+    //repopulate DOM with
+    // sorted data
+    todos.forEach(function (todo) {
+
+        const todoDiv = document.createElement("div");
+        todoDiv.classList.add("todo");
+
+        //Create LI
+        const newTodo = document.createElement("li");
+        newTodo.innerText = todo;
+        newTodo.classList.add("todo-item");
+        todoDiv.appendChild(newTodo);
+
+        //checked button
+        const completedButton = document.createElement("button");
+        completedButton.innerHTML = '<i class="fas fa-check"></i>';
+        completedButton.classList.add("complete-btn");
+        todoDiv.appendChild(completedButton);
+        //Delete button
+        const trashButton = document.createElement("button");
+        trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+        trashButton.classList.add("trash-btn");
+        todoDiv.appendChild(trashButton);
+
+        //append to list(attaching to actual UL)
+        todoList.appendChild(todoDiv);
+
+    });
+
+   
+    todos = JSON.stringify(todos)
+    localStorage.setItem("todos", todos);
+
+
 }
-
-
 
 //functions
 
@@ -139,27 +177,27 @@ function getTodos() {
 
 
 
-        //append to list(attaching to actual UL)
+        //append to do list(attaching to actual UL)
         todoList.appendChild(todoDiv);
 
     });
 }
 
-/* EDIT LIST ITEMS*/
+/*EDIT LIST ITEMS*/
 let todolistitem; // Element being edited
 
 let listActions = {
-  editItem: function() {
-    currentElement.innerHTML = currentElement.innerHTML.replace(currentElement.innerText, editInput.value);
-    editInput.value = '';
-  }
+    editItem: function () {
+        currentElement.innerHTML = currentElement.innerHTML.replace(currentElement.innerText, editInput.value);
+        editInput.value = '';
+    }
 }
 
 let modalControls = {
-  editModal: function(i)  {
-    // *i* is *this* in *modalControls.editModal(this)*
-    modalContainer.style.display = 'block';
-    currentElement = i.parentElement;
-    modalContainer.getElementsByTagName('input')[0].value = currentElement.innerText;
-  }
+    editModal: function (i) {
+        modalContainer.style.display = 'block';
+        currentElement = i.parentElement;
+        modalContainer.getElementsByTagName('input')[0].value = currentElement.innerText;
+    }
 }
+
